b3d880350e485df19343ef8569f46ecff2b9bda2
amanjadon54 <amanjadon54@gmail.com>
2020-06-01 17:06:01 +0530

    Core: subset target sum/ subset equal sum partition

--- src/com/preparation/algorithm/dp/grokkingDPPattern/knapsack01/SubsetSum.java
+++ src/com/preparation/algorithm/dp/grokkingDPPattern/knapsack01/SubsetSum.java
@@ -1,4 +1,77 @@
 package com.preparation.algorithm.dp.grokkingDPPattern.knapsack01;
 
+import java.util.Arrays;
+
+/**
+ * Subset Sum Problem | DP-25
+ * Given a set of non-negative integers, and a value sum, determine if there is a subset of the given set with sum equal to given sum.
+ * Example:
+ * <p>
+ * Input: set[] = {3, 34, 4, 12, 5, 2}, sum = 9
+ * Output: True
+ * There is a subset (4, 5) with sum 9.
+ * <p>
+ * Input: set[] = {3, 34, 4, 12, 5, 2}, sum = 30
+ * Output: False
+ * There is no subset that add up to 30.
+ *
+ *
+ * Approach :
+ * Same logic as used in EqualSubsetSumPartition
+ */
 public class SubsetSum {
+
+    public static boolean canPartition(int[] nums, int sum) {
+
+        int[][] memo = new int[nums.length + 1][sum + 1];
+        for (int[] array : memo) {
+            Arrays.fill(array, -1);
+        }
+        return canPartition(nums, sum, 0, 0, memo);
+    }
+
+
+    static boolean canPartition(int[] nums, int k, int index, int sum, int[][] memo) {
+
+        if (sum == k) {
+            return true;
+        }
+
+        if (index >= nums.length) {
+            return false;
+        }
+
+        if (memo[index][sum] != -1) {
+            if (memo[index][sum] == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+
+        boolean included = false;
+        if (sum + nums[index] <= k) {
+            included = canPartition(nums, k, index + 1, sum + nums[index], memo);
+        }
+
+        boolean excluded = canPartition(nums, k, index + 1, sum, memo);
+
+        boolean valid = included | excluded;
+        if (valid) {
+            memo[index][sum] = 1;
+        } else {
+            memo[index][sum] = 0;
+        }
+
+        return included | excluded;
+    }
+
+
+    public static void main(String... s) {
+        int nums[] = new int[]{1, 5, 11, 5};
+        int sum = 11;
+        System.out.println(canPartition(nums, sum)
+        );
+    }
 }
--- src/com/preparation/algorithm/dp/grokkingDPPattern/knapsack01/TargetSSum.java
+++ src/com/preparation/algorithm/dp/grokkingDPPattern/knapsack01/TargetSSum.java
@@ -1,4 +1,67 @@
 package com.preparation.algorithm.dp.grokkingDPPattern.knapsack01;
 
+import java.util.HashMap;
+
+/**
+ * You are given a list of non-negative integers, a1, a2, ..., an, and a target, S. Now you have 2 symbols + and -.
+ * For each integer, you should choose one from + and - as its new symbol.
+ * <p>
+ * Find out how many ways to assign symbols to make sum of integers equal to target S.
+ * <p>
+ * Example 1:
+ * Input: nums is [1, 1, 1, 1, 1], S is 3.
+ * Output: 5
+ * Explanation:
+ * <p>
+ * -1+1+1+1+1 = 3
+ * +1-1+1+1+1 = 3
+ * +1+1-1+1+1 = 3
+ * +1+1+1-1+1 = 3
+ * +1+1+1+1-1 = 3
+ * <p>
+ * There are 5 ways to assign symbols to make the sum of nums be target 3.
+ */
 public class TargetSSum {
+
+    public static void main(String... s) {
+        int nums[] = new int[]{1, 1, 1, 1, 1};
+        int k = 3;
+        System.out.println(findTargetSum(nums, k)
+        );
+    }
+
+    static long count;
+
+    public static long findTargetSum(int[] nums, int k) {
+        HashMap<String, Long> memo = new HashMap();
+
+        return findTargetSum(nums, k, 0, 0, memo);
+
+    }
+
+    private static long findTargetSum(int[] nums, int k, int index, int sum, HashMap<String, Long> memo) {
+
+        String key = index + "-" + sum;
+
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+
+        if (index == nums.length) {
+            if (sum == k) {
+                return 1;
+            }
+            return 0;
+        }
+
+
+        long included = findTargetSum(nums, k, index + 1, sum + nums[index], memo);
+        long excluded = findTargetSum(nums, k, index + 1, sum - nums[index], memo);
+
+        memo.put(key, included + excluded);
+
+        return included + excluded;
+
+    }
+
 }
