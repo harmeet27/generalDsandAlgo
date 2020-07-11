ade53f6c336994a8c2718dec3b3e0d1ee2191fd5
amanjadon54 <amanjadon54@gmail.com>
2020-07-11 18:47:03 +0530

    Core: added node support

---
+++ node/src/driver.js
@@ -0,0 +1,11 @@
+import LinkedList from './LinkedList.js';
+
+let list = new LinkedList();
+list.add('2');
+list.add('3');
+list.add('4');
+list.add('5');
+list.add('1', 1);
+list.add('6', 0);
+// list.remove('2');
+console.log('list', list);
