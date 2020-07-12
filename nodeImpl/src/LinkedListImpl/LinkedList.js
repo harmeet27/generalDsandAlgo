f568283c678331f040eb8295b6b8e675cfee92f3
harmeet27 <harmeetkaur2793@gmail.com>
2020-07-12 12:03:57 +0530

    chore: update folder structure

---
+++ nodeImpl/src/LinkedListImpl/LinkedList.js
@@ -0,0 +1,114 @@
+import Node from './Node.js';
+
+class LinkedList{
+constructor(){
+  this.head = null;
+}
+
+ add(data, index = null){
+   if(index !== null){
+     return this._addAtIndex(data, index);
+   }
+   if(this.head === null){
+     this.head = new Node(data, null);
+   }
+   else{
+   let temp = this.head;
+   while(temp !== null){
+     if(temp.next === null){
+       const node = new Node(data, null);
+       temp.next = node;
+       break;
+     }
+     temp = temp.next;
+   }
+  }
+   return this.head;
+ }
+
+ _addAtFront(data){
+   const node = new Node(data, this.head);
+   this.head = node;
+   return this.head;
+ }
+
+ _addAtIndex(data, index){
+   let temp = this.head;
+   let previous = null;
+   let count = 0
+   while(temp !== null){
+     if(count === index){
+       const node = new Node(data, null);
+       if(index === 0){
+         return this._addAtFront(data);
+       }
+       previous.next = node;
+       node.next = temp;
+     }
+     count = count + 1;
+     previous = temp;
+     temp = temp.next;
+   }
+
+   if(temp === null && index === count){
+     return this.add(data);
+   }
+
+   if(temp === null && index >= count){
+     throw new Error('index out of bound');
+   }
+   return this.head;
+ }
+
+ remove(data, index = null){
+   if(this.head.data === data || index === 0){
+     this.head = this.head.next;
+     return this.head;
+   }
+   let temp = this.head;
+   let previous = temp;
+   while(temp !== null){
+     if(temp.data === data){
+         previous.next = temp.next;
+     }
+     previous = temp;
+     temp = temp.next;
+   }
+   return this.head;
+ }
+
+ removeAtIndex(index){
+     if(index === 0){
+       this.remove(data, index);
+     }
+     let temp = this.head;
+     let previous = null;
+     let count = 0;
+     while(temp !== null){
+       if(index === count){
+         previous.next = temp.next;
+         break;
+       }
+       previous = temp;
+       temp = temp.next;
+       count++;
+     }
+     return this.head;
+ }
+
+ reverse(){
+   let current = this.head;
+   let previous = null;
+   let next = null;
+   while(current !== null){
+     next = current.next;
+     current.next = previous;
+     previous = current;
+     current = next;
+   }
+   this.head = previous;
+   return this.head;
+ }
+}
+
+export default LinkedList;
---
+++ nodeImpl/src/driver.js
@@ -0,0 +1,17 @@
+import LinkedList from './LinkedListImpl/LinkedList.js';
+
+let list = new LinkedList();
+list.add('2');
+list.add('3');
+list.add('4');
+list.add('5');
+list.add('1', 1);
+list.add('6', 0);
+list.add('8', 3);
+list.removeAtIndex(1);
+// list.remove('2');
+console.log('list', list);
+
+list.reverse()
+
+console.log('reverse', list);
