12c38df47c520ade7de51a47670b91dfb0e5aad1
harmeet27 <harmeetkaur2793@gmail.com>
2020-07-25 17:33:31 +0530

    fix: dutchFlag reduce variables

--- nodeImpl/src/Algorithms/dutchFlag.js
+++ nodeImpl/src/Algorithms/dutchFlag.js
@@ -2,35 +2,34 @@ function dutchFlag(inputArray){
   const set = new Set(inputArray);
   const entries = set.values();
   const entriesArray = [];
+  const entryCountArray = [];
+  const fullSortedArray = [];
   for(const entry of entries){
     entriesArray.push(entry);
+    entryCountArray.push(0);
   }
-  let countOfFirstEntry = 0;
-  let countOfSecondEntry= 0;
-  let countOfThirdEntry = 0;
   const sortedEntriesArray = entriesArray.sort();
   inputArray.forEach((element) => {
     if(element === sortedEntriesArray[0]){
-      countOfFirstEntry = countOfFirstEntry + 1;
+      entryCountArray[0] = entryCountArray[0] + 1;
     }
     else if(element === sortedEntriesArray[1]){
-      countOfSecondEntry = countOfSecondEntry + 1;
+      entryCountArray[1] = entryCountArray[1] + 1;
     }
     else if(element === sortedEntriesArray[2]){
-      countOfThirdEntry = countOfThirdEntry + 1;
+      entryCountArray[2] = entryCountArray[2] + 1;
     }
   })
- const fullSortedArray = [];
- while(countOfFirstEntry !== 0 || countOfSecondEntry !== 0 || countOfThirdEntry !== 0){
-   if(countOfFirstEntry > 0){
+ while(entryCountArray[0] !== 0 || entryCountArray[1] !== 0 || entryCountArray[2] !== 0){
+   if(entryCountArray[0] > 0){
      fullSortedArray.push(sortedEntriesArray[0]);
-     countOfFirstEntry = countOfFirstEntry - 1;
-   } else if ( countOfSecondEntry > 0){
+     entryCountArray[0] = entryCountArray[0] - 1;
+   } else if ( entryCountArray[1] > 0){
      fullSortedArray.push(sortedEntriesArray[1]);
-     countOfSecondEntry = countOfSecondEntry - 1;
-   } else if ( countOfThirdEntry > 0){
+     entryCountArray[1] = entryCountArray[1] - 1;
+   } else if ( entryCountArray[2] > 0){
      fullSortedArray.push(sortedEntriesArray[2]);
-     countOfThirdEntry = countOfThirdEntry - 1;
+     entryCountArray[2] = entryCountArray[2] - 1;
    }
  }
  return fullSortedArray;
