580ee8561d8533a8767e092b3a9411ab86a7cf52
amanjadon54 <amanjadon54@gmail.com>
2021-05-26 17:47:02 +0530

    Core: quick sort understanding added

--- java/src/com/preparation/algorithm/sorting/techniques/QuickSort.java
+++ java/src/com/preparation/algorithm/sorting/techniques/QuickSort.java
@@ -1,55 +1,34 @@
 package com.preparation.algorithm.sorting.techniques;
 
-import java.util.Arrays;
-
-/**
- * 1. Find pivotElement
- * 2. find from left, larger than pivot, and from right smaller.
- * 3. of l<r swap l,r --> keep pivot element as it is even if it has been swapped.
- * 4. if not you are out of loop and divide it into 2 arrays. At this time :
- *      When the right bound crosses the left bound, all elements to the left of the left bound are
- *      less than the pivot and all elements to the right are greater than or equal to the pivot.
- *
- *      which is why at this point we swap l,pivot and divide here. Here pivot gets fixed
- */
-
-
 public class QuickSort {
-    void sort(int[] arr) {
-        findK(arr, 0, arr.length - 1);
-        printArr(arr);
+
+    private void quickSort(int[] arr, int low, int high) {
+        if (low >= high) return;
+
+        if (low >= 0 && high < arr.length && high >= 0 && low < arr.length) {
+            int pivotIndex = quick(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
     }
 
-    public void findK(int arr[], int l, int r) {
-        int start = l;
-        int end = r;
-        int pivotIndex = start + (end - start) / 2;
-        int pivot = arr[pivotIndex];
+    private int quick(int[] arr, int l, int r) {
+        int pivotIndex = r;
+        int pivotElement = arr[r];
+
         while (l <= r) {
-            while (arr[l] < pivot) {
-                l++;
-            }
-
-            while (arr[r] > pivot) {
-                r--;
-            }
-
+            while (l < arr.length && arr[l] < pivotElement) l++;
+            while (r >= 0 && arr[r] >= pivotElement) r--;
             if (l <= r) {
                 swap(l, r, arr);
-                l++;
-                r--;
+            } else {
+                swap(l, pivotIndex, arr);
+                //found the pivotElement index after which we can divide.
+                pivotIndex = l;
+                break;
             }
         }
-
-        //i.e. l>r whenever l crosses r, then the left elements of l will  be smaller than pivot
-        //and right elemtns of l will be greater than pivot.
-        if (l < end) {
-            findK(arr, l, end);
-        }
-        if (r > start) {
-            findK(arr, start, r);
-        }
-
+        return pivotIndex;
     }
 
     private void swap(int l, int r, int arr[]) {
@@ -58,14 +37,11 @@ public class QuickSort {
         arr[r] = temp;
     }
 
-    private void printArr(int[] arr) {
-        Arrays.stream(arr).forEach(x -> System.out.println(x));
-    }
-
-
     public static void main(String... s) {
-        QuickSort quickSort = new QuickSort();
-        int arr[] = new int[]{8, 4, 3, 2, 1, 9, 11, 2, 5};
-        quickSort.sort(arr);
+        QuickSort adv = new QuickSort();
+        int arr[] = {5, 2, 3, 1};
+        adv.quickSort(arr, 0, arr.length - 1);
     }
+
+
 }
